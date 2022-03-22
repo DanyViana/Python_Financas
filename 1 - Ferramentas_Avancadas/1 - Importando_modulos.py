084ae48891863dccf70656e66d383f6a23b0c6dc
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2022-03-22 11:46:22 -0300

    Create 1 - Importando_modulos.py
    biblioteca math

---
+++ 1 - Ferramentas_Avancadas/1 - Importando_modulos.py
@@ -0,0 +1,26 @@
+# Para importar a bibliote math, que será utilizada em cálculos matemáticos, você pode realizar de algumas formas: 
+
+#Forma mais comum é importando toda a biblioteca: 
+import math 
+raiz = math.sqrt(16) #O nome do módulo e o nome da função, nesse caso é o sqrt (raíz quadrada)
+print (raiz)
+
+#Se você souber que vai utilizar apenas a raíz quadrada, é possível fazer somente dessa forma: 
+from math import sqrt
+raiz2 = sqrt(25)
+print (raiz2)
+
+#A que os especilistas mais gostam e é utilizada em códigos mais complexos pois economiza palavras são essas duas formas:
+from math import sqrt as s
+raiz3a = s(36)
+print (raiz3a)
+
+import math as m
+raiz3b = m.sqrt(49)
+print (raiz3b)
+
+#A última é muito desaprovada por especialistas:
+from math import * #Vai importar tudo da biblioteca math
+raiz4 = sqrt(64)
+print (raiz4)
+#Essa opção pode ser problemática principalmente se você importar outros módulos que tenham a mesma função que você está trabalhando, o python irá escolher a que ele quiser. 
