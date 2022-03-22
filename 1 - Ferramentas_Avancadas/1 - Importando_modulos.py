e78c77415904d9a7a02df9d143370dd1ffff3127
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2022-03-22 13:32:55 -0300

    Update 1 - Importando_modulos.py
    atualização

--- 1 - Ferramentas_Avancadas/1 - Importando_modulos.py
+++ 1 - Ferramentas_Avancadas/1 - Importando_modulos.py
@@ -24,3 +24,19 @@ from math import * #Vai importar tudo da biblioteca math
 raiz4 = sqrt(64)
 print (raiz4)
 #Essa opção pode ser problemática principalmente se você importar outros módulos que tenham a mesma função que você está trabalhando, o python irá escolher a que ele quiser. 
+
+#Biblioteca "numpy" é um pacote de terceiros que nos permite trabalhar com arrays (organizar e processar dados) multidimensionais. 
+
+#Biblioteca "pandas" nos permite organizar os dados em uma forma tabular e anexar rótulos descritivos as linhas e colunas da tabela. Ótimo para trabalhar com séries temporais e banco de dados enormes. 
+
+#Biblioteca "matplotlib" é de gráficos bidimensionais especialmente projetada para visualização de cálculos numpay.
+
+#Biblioteca "math" para funções matemáticas.
+
+#Biblioteca "random" é para gerar números aleatórios.
+
+#Biblioteca "statsmodels" é para estatística descritiva e plotagem de gráficos.
+
+#Todas essas bibliotecas já estão instaladas no pacote do Anaconda.
+
+#Para instalar alguma biblioteca é só ir no menu inicial, procurar por "anaconda prompt", digite "pip install (nome da biblioteca)" - pip install pandas_datareader e apertar enter. Prontinho! 
